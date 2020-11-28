fa1db50c657366c1dc23696fb17edd7bf91df642
Egor Ermolaev <probegrofanston@mail.ru>
2020-11-28 13:32:41 +0300

    fix Cart.test

--- src/components/Cart/__tests__/Cart.test.tsx
+++ src/components/Cart/__tests__/Cart.test.tsx
@@ -1,18 +1,23 @@
-// types
-import {IProduct} from 'models/interfaces';
-
 import React from 'react';
 import {MemoryRouter} from 'react-router';
 import {shallow, mount} from 'enzyme';
 import {products} from 'static/__test__/card';
 import {Cart, IProps} from 'components/Cart/Cart';
-import {ButtonIconed} from '../../Common/Buttons/ButtonIconed';
+import {ButtonIconed} from 'components/Common/Buttons/ButtonIconed';
 
 const shallowCartComponent = (props: IProps) => (
   shallow(
     <Cart {...props} />
   )
 );
+
+const mountCartComponent = (props: IProps, isRouter: boolean) => {
+  if (isRouter) {
+    return mount(<MemoryRouter><Cart {...props} /></MemoryRouter>);
+  } else {
+    return mount(<Cart {...props} />);
+  }
+}
 
 describe('Cart', () => {
   let props: IProps;
@@ -23,19 +28,19 @@ describe('Cart', () => {
       total: 1,
       countProducts: 1,
       items: products,
-      removeFromCart: (product) => {},
+      removeFromCart: () => {},
     };
     popupVisible = false;
   });
 
   it('Match snapshot', () => {
-    const component = mount(<MemoryRouter><Cart {...props} /></MemoryRouter>);
+    const component = mountCartComponent(props, true);
 
     expect(component).toMatchSnapshot();
   });
 
   it('Should change state', () => {
-    const component = mount(<Cart {...props} />);
+    const component = mountCartComponent(props, false);
     const button = component.find(ButtonIconed);
     button.simulate('click', {
       popupVisible: true,
