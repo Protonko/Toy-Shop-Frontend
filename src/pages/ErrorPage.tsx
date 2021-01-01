1bb1e441fefca7025a3bd168d34b931aedf0593b
Egor Ermolaev <probegrofanston@mail.ru>
2021-01-01 18:38:33 +0300

    add error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,12 @@
 import React, {FC} from 'react';
+import {ReactComponent as NotFound} from 'assets/icons/404.svg';
 
 export const ErrorPage: FC = () => {
   return (
-    <div>404</div>
+    <div className="error-page">
+      <div className="error-page__logo">
+        <NotFound />
+      </div>
+    </div>
   )
 };
